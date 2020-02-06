353c688ec1d13e932b44e33904fcab44739ba364
bknight-i3drobotics <bknight@i3drobotics.com>
2020-02-06 16:09:22 +0000

    add samples for deimos and phobos. Update to sample script

--- pyStereo3D/SampleScripts/sample_script.py
+++ pyStereo3D/SampleScripts/sample_script.py
@@ -7,13 +7,21 @@ CAMERA_TYPE_IC = 1
 CAMERA_TYPE_OPENCV_SPLIT = 2
 CAMERA_TYPE_OPENCV_DUAL = 3
 CAMERA_TYPE_OPENCV_IMAGE = 4
+CAMERA_TYPE_OPENCV_IMAGE_RECT = 5
 CAMERA_TYPE_OPENCV_FOLDER = 5
 
 # select camera capture type
-camera_type = CAMERA_TYPE_OPENCV_IMAGE
+camera_type = CAMERA_TYPE_OPENCV_IMAGE_RECT
+
+# define output folder
+folder = "pyStereo3D/SampleData/"
+# define calibration files for left and right image
+left_cal_file = folder + "deimos_left.yaml"
+right_cal_file = folder + "deimos_right.yaml"
 
 # setup stereo camera
 stcam = None
+isRectified = False
 if (camera_type == CAMERA_TYPE_PYLON): # Pylon camera capture
     left_camera_serial = "22864917"
     right_camera_serial = "22864912"
@@ -31,12 +39,19 @@ elif (camera_type == CAMERA_TYPE_OPENCV_DUAL): # OpenCV (One camera has left and
     cam = CVCapture(0)
     stcam = StereoCaptureCVDual(cam)
 elif (camera_type == CAMERA_TYPE_OPENCV_IMAGE): # Read left and right from image
-    camL = CVImageCapture("pyStereo3D/SampleData/left.png")
-    camR = CVImageCapture("pyStereo3D/SampleData/right.png")
+    camL = CVImageCapture("pyStereo3D/SampleData/deimos_left.png")
+    camR = CVImageCapture("pyStereo3D/SampleData/deimos_right.png")
+    stcam = StereoCaptureCVSplit(camL,camR)
+elif (camera_type == CAMERA_TYPE_OPENCV_IMAGE_RECT): # Read left and right from image
+    isRectified = True
+    left_cal_file = folder + "phobos_left.yaml"
+    right_cal_file = folder + "phobos_right.yaml"
+    camL = CVImageCapture("pyStereo3D/SampleData/phobos_rect_left.png")
+    camR = CVImageCapture("pyStereo3D/SampleData/phobos_rect_right.png")
     stcam = StereoCaptureCVSplit(camL,camR)
 elif (camera_type == CAMERA_TYPE_OPENCV_FOLDER): # Read left and right from images in folders (MUST be in seperate folders)
-    camL = CVImageFolderCapture("pyStereo3D/SampleData/left/")
-    camR = CVImageFolderCapture("pyStereo3D/SampleData/right/")
+    camL = CVImageFolderCapture("pyStereo3D/SampleData/deimos_left/")
+    camR = CVImageFolderCapture("pyStereo3D/SampleData/deimos_right/")
     stcam = StereoCaptureCVSplit(camL,camR)
 else:
     print("Invalid camera type.")
@@ -45,13 +60,7 @@ else:
 # load stereo camera into generic stereo camera capture class
 stcap = StereoCapture(stcam)
 
-# define inout folder
-folder = "pyStereo3D/SampleData/"
-# define calibration files for left and right image
-left_cal_file = folder + "left.yaml"
-right_cal_file = folder + "right.yaml"
-
 # setup Stereo3D
 s3D = Stereo3D(left_cal_file,right_cal_file,stcap)
 # run Stereo3D GUI for generating 3D
-s3D.run(folder)
+s3D.run(folder,isRectified=isRectified) #if input images are already rectified then set 'isRectified' to True
