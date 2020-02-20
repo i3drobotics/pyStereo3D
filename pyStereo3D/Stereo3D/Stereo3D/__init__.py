148d7cf935dbafd84434a6ff3353f57e68e840aa
bknight-i3drobotics <bknight@i3drobotics.com>
2020-02-20 17:48:14 +0000

    add stereo calibration

--- pyStereo3D/Stereo3D/Stereo3D/__init__.py
+++ pyStereo3D/Stereo3D/Stereo3D/__init__.py
@@ -1,13 +1,15 @@
 from StereoCapture import StereoCapture
+from Stereo3D.StereoCalibration import StereoCalibration
 import numpy as np
 import cv2
 import os
 import time
+import glob
 from pymsgbox import *
 
 class Stereo3D():
     
-    def __init__(self,left_cal_file,right_cal_file,stereo_camera=None):
+    def __init__(self,stereo_camera,stereo_calibration,stereo_matcher=None):
         """
         Initialisation function for PickPlace3D. Defines the devices used.
         If you would like to use the methods without connecting a camera then initaialise Stereo3D as 's3D = Stereo3D()'.
@@ -18,9 +20,9 @@ class Stereo3D():
         :type right_cal_file: string
         :type stereo_camera: StereoCapture.StereoCapture
         """
-        self.stereo_camera = stereo_camera
-        self.calLoaded, self.m_l, self.m_r, self.d_l, self.d_r, self.r_r, self.r_l, self.p_r, self.p_l = self.get_cal_from_file(left_cal_file,right_cal_file)
-        self.Q = self.calc_q(self.m_l, self.p_r, self.p_l)
+        self.change_camera(stereo_camera)
+        self.stereo_calibration = stereo_calibration
+        self.Q = self.stereo_calibration.stereo_cal["q"]
 
         self.image_left = None
         self.image_right = None
@@ -35,40 +37,11 @@ class Stereo3D():
         cv2.namedWindow(self.cv_window_name_Controls,cv2.WINDOW_NORMAL)
         cv2.namedWindow(self.cv_window_name_Images,cv2.WINDOW_NORMAL)
 
-        default_min_disp = 1000
-        default_num_disparities = 20
-        default_block_size = 12
-        default_uniqueness_ratio = 15
-        default_texture_threshold = 15
-        default_speckle_size = 0
-        default_speckle_range = 500
-
-        cv2.createTrackbar("Min disp", self.cv_window_name_Controls , default_min_disp, 2000, self.on_min_disparity_trackbar)
-        cv2.createTrackbar("Disp", self.cv_window_name_Controls , default_num_disparities, 30, self.on_num_disparities_trackbar)
-        cv2.createTrackbar("Blck sze", self.cv_window_name_Controls , default_block_size, 100, self.on_block_size_trackbar)
-
-        cv2.createTrackbar("Uniq", self.cv_window_name_Controls , default_uniqueness_ratio, 100, self.on_uniqueness_ratio_trackbar)
-        cv2.createTrackbar("Texture", self.cv_window_name_Controls , default_texture_threshold, 100, self.on_texture_threshold_trackbar)
-
-        cv2.createTrackbar("Sp size", self.cv_window_name_Controls , default_speckle_size, 30, self.on_speckle_size_trackbar)
-        cv2.createTrackbar("Sp range", self.cv_window_name_Controls , default_speckle_range, 1000, self.on_speckle_range_trackbar)
-
         cv2.setMouseCallback(self.cv_window_name_Images, self.on_window_mouse)
 
         cv2.resizeWindow(self.cv_window_name_Controls, 400,0 )
 
-        # Once init has been called matcher can be changed e.g.
-        # s3D = Stereo3D()
-        # s3D.matcher = cv2.StereoSGBM_create()
-        self.matcher = cv2.StereoBM_create()
-        calc_block = (2 * default_block_size + 5)
-        self.matcher.setBlockSize(calc_block)
-        self.matcher.setMinDisparity(int(default_min_disp - 1000))
-        self.matcher.setNumDisparities(16*(default_num_disparities+1))
-        self.matcher.setUniquenessRatio(default_uniqueness_ratio)
-        self.matcher.setTextureThreshold(default_texture_threshold)
-        self.matcher.setSpeckleWindowSize(default_speckle_size)
-        self.matcher.setSpeckleRange(default_speckle_range)
+        self.change_matcher(stereo_matcher)
 
         # Once init has been called matcher can be changed e.g.
         # s3D = Stereo3D()
@@ -85,6 +58,66 @@ class Stereo3D():
             "property uchar blue\n"
             "end_header\n")
 
+    def change_camera(self,stereo_camera):
+        self.stereo_camera = stereo_camera
+
+    def change_matcher(self,stereo_matcher):
+        default_min_disp = 1000
+        default_num_disparities = 20
+        default_block_size = 12
+        default_uniqueness_ratio = 15
+        default_texture_threshold = 15
+        default_speckle_size = 0
+        default_speckle_range = 500
+
+        if (stereo_matcher is None):
+            self.matcher = cv2.StereoBM_create()
+            calc_block = (2 * default_block_size + 5)
+            self.matcher.setBlockSize(calc_block)
+            self.matcher.setMinDisparity(int(default_min_disp - 1000))
+            self.matcher.setNumDisparities(16*(default_num_disparities+1))
+            self.matcher.setUniquenessRatio(default_uniqueness_ratio)
+            self.matcher.setTextureThreshold(default_texture_threshold)
+            self.matcher.setSpeckleWindowSize(default_speckle_size)
+            self.matcher.setSpeckleRange(default_speckle_range)
+        else:
+            if stereo_matcher == "BM":
+                self.matcher = cv2.StereoBM_create()
+                calc_block = (2 * default_block_size + 5)
+                self.matcher.setBlockSize(calc_block)
+                self.matcher.setMinDisparity(int(default_min_disp - 1000))
+                self.matcher.setNumDisparities(16*(default_num_disparities+1))
+                self.matcher.setUniquenessRatio(default_uniqueness_ratio)
+                self.matcher.setTextureThreshold(default_texture_threshold)
+                self.matcher.setSpeckleWindowSize(default_speckle_size)
+                self.matcher.setSpeckleRange(default_speckle_range)
+            elif stereo_matcher == "SGBM":
+                self.matcher = cv2.StereoSGBM_create()
+                calc_block = (2 * default_block_size + 5)
+                self.matcher.setBlockSize(calc_block)
+                self.matcher.setMinDisparity(int(default_min_disp - 1000))
+                self.matcher.setNumDisparities(16*(default_num_disparities+1))
+                self.matcher.setUniquenessRatio(default_uniqueness_ratio)
+                #self.matcher.setTextureThreshold(default_texture_threshold)
+                self.matcher.setSpeckleWindowSize(default_speckle_size)
+                self.matcher.setSpeckleRange(default_speckle_range)
+            else:
+                self.matcher = stereo_matcher
+
+        cv2.destroyWindow(self.cv_window_name_Controls)
+        cv2.namedWindow(self.cv_window_name_Controls,cv2.WINDOW_NORMAL)
+
+        cv2.createTrackbar("Min disp", self.cv_window_name_Controls , default_min_disp, 2000, self.on_min_disparity_trackbar)
+        cv2.createTrackbar("Disp", self.cv_window_name_Controls , default_num_disparities, 30, self.on_num_disparities_trackbar)
+        cv2.createTrackbar("Blck sze", self.cv_window_name_Controls , default_block_size, 100, self.on_block_size_trackbar)
+
+        cv2.createTrackbar("Uniq", self.cv_window_name_Controls , default_uniqueness_ratio, 100, self.on_uniqueness_ratio_trackbar)
+        if stereo_matcher == "BM":
+            cv2.createTrackbar("Texture", self.cv_window_name_Controls , default_texture_threshold, 100, self.on_texture_threshold_trackbar)
+
+        cv2.createTrackbar("Sp size", self.cv_window_name_Controls , default_speckle_size, 30, self.on_speckle_size_trackbar)
+        cv2.createTrackbar("Sp range", self.cv_window_name_Controls , default_speckle_range, 1000, self.on_speckle_range_trackbar)
+
     def connect(self):
         """
         Connect to devices needed for the pick and place process.
@@ -93,58 +126,6 @@ class Stereo3D():
         """
         res = self.stereo_camera.connect()
         return res
-
-    def get_cal_from_file(self, left_cal_file, right_cal_file):
-        res = False
-        fs_l = cv2.FileStorage(left_cal_file, cv2.FILE_STORAGE_READ)
-        fs_r = cv2.FileStorage(right_cal_file, cv2.FILE_STORAGE_READ)
-        if fs_l.isOpened() and fs_r.isOpened():
-            m_l = fs_l.getNode("camera_matrix").mat()
-            m_r = fs_r.getNode("camera_matrix").mat()
-            d_l = fs_l.getNode("distortion_coefficients").mat()
-            d_r = fs_r.getNode("distortion_coefficients").mat()
-            r_l = fs_l.getNode("rectification_matrix").mat()
-            r_r = fs_r.getNode("rectification_matrix").mat()
-            p_l = fs_l.getNode("projection_matrix").mat()
-            p_r = fs_r.getNode("projection_matrix").mat()
-            res = True
-        else:
-            print("Failed to open calibration files")
-            return False, None, None, None, None, None, None, None, None
-        fs_l.release()
-        fs_r.release()
-        return res, m_l, m_r, d_l, d_r, r_r, r_l, p_r, p_l
-
-    def calc_q(self, m_l, p_r, p_l):
-        q = np.zeros(shape=(4,4))
-        self.cx = p_l[0,2]
-        self.cxr = p_r[0,2]
-        self.cy = p_l[1,2]
-        self.fx = m_l[0,0]
-        self.fy = m_l[1,1]
-
-        p14 = p_r[0,3]
-        self.T = -p14 / self.fx
-
-        q33 = -(self.cx - self.cxr)/self.T
-
-        # calucalte Q values
-        q[0,0] = 1.0
-        q[0,3] = -self.cx
-        q[1,1] = 1.0
-        q[1,3] = -self.cy #501
-        q[2,3] = self.fx
-        q[3,2] = 1.0 / self.T
-        q[3,3] = q33
-
-        return q
-
-    def rectify(self,left_image,right_image):
-        mapL1, mapL2 = cv2.initUndistortRectifyMap(self.m_l, self.d_l, self.r_l, self.p_l, left_image.shape[::-1], cv2.CV_32FC1)
-        mapR1, mapR2 = cv2.initUndistortRectifyMap(self.m_r, self.d_r, self.r_r, self.p_r, right_image.shape[::-1], cv2.CV_32FC1)
-        rect_image_l = cv2.remap(left_image, mapL1, mapL2, cv2.INTER_LINEAR)
-        rect_image_r = cv2.remap(right_image, mapR1, mapR2, cv2.INTER_LINEAR)
-        return rect_image_l, rect_image_r
 
     def gen3D(self,left_image, right_image):
         disparity = self.matcher.compute(left_image,right_image).astype(np.float32) / 16.0
@@ -211,25 +192,10 @@ class Stereo3D():
             if (isRectified):
                 self.rect_image_left, self.rect_image_right = self.image_left, self.image_right
             else:
-                self.rect_image_left, self.rect_image_right = self.rectify(image_left, image_right)
+                self.rect_image_left, self.rect_image_right = self.stereo_calibration.rectify_pair(image_left,image_right)
 
             disp = self.gen3D(self.rect_image_left, self.rect_image_right)
             self.disparity = disp
-
-            display_image = np.zeros((640, 480), np.uint8)
-
-            rect_image_left_resized = cv2.resize(self.rect_image_left, (1280, 960), interpolation = cv2.INTER_AREA)
-            rect_image_right_resized = cv2.resize(self.rect_image_right, (1280, 960), interpolation = cv2.INTER_AREA)
-
-            disp_resized = self.scale_disparity(cv2.resize(disp, (1280, 960), interpolation = cv2.INTER_AREA))
-            left_right_dual = np.concatenate((rect_image_left_resized, rect_image_right_resized), axis=1)
-            disp_spacer = np.zeros((960, 640), np.uint8)
-            disp_spaced = np.concatenate((disp_spacer, disp_resized, disp_spacer), axis=1)
-            display_image = np.concatenate((disp_spaced, left_right_dual), axis=0)
-
-            display_image_resize = cv2.resize(display_image, (1280, 1280), interpolation = cv2.INTER_AREA)
-            
-            cv2.imshow(self.cv_window_name_Images, display_image_resize)
 
             return True, disp
         else:
@@ -252,37 +218,71 @@ class Stereo3D():
             alert('3D point cloud saved.', 'Save 3D Point Cloud')
 
     def run(self,defaultSaveFolder="",isRectified=False,frame_delay=0):
-        if (self.calLoaded):
-            # connect to stereo camera
-            self.connect()
-            save_index = 0
-            while(True):
-                # grab 3D disparity from stereo camera
-                res, disp = self.grab3D(isRectified)
-                k = cv2.waitKey(1)          
-                if k == ord('q'): # exit if 'q' key pressed
-                    break
-                elif k == ord('s'): # save stereo image pair
-                    left_file_string=str(save_index)+"_l.png"
-                    right_file_string=str(save_index)+"_r.png"
-                    self.stereo_camera.save_images(self.image_left,self.image_right,defaultSaveFolder,left_file_string,right_file_string)
-                    save_index += 1
-                elif k == ord('r'): # save rectified stereo image pair
-                    left_file_string="rect_"+str(save_index)+"_l.png"
-                    right_file_string="rect_"+str(save_index)+"_r.png"
-                    self.stereo_camera.save_images(self.rect_image_left,self.rect_image_right,defaultSaveFolder,left_file_string,right_file_string)
-                    save_index += 1
-                elif k == ord('p'): # save 3D data as point cloud
-                    points_file_string = "points_"+str(save_index)+".ply"
-                    self.save_point_cloud(disp,self.rect_image_left,defaultSaveFolder,points_file_string)
-                    save_index += 1
-                if cv2.getWindowProperty(self.cv_window_name_Images,cv2.WND_PROP_VISIBLE) < 1:        
-                    break
-                if cv2.getWindowProperty(self.cv_window_name_Controls,cv2.WND_PROP_VISIBLE) < 1:        
-                    break
-                time.sleep(frame_delay)
-        else:
-            print("Failed to calibration files so cannot continue.")
+        # connect to stereo camera
+        self.connect()
+        save_index = 0
+        while(True):
+            # grab 3D disparity from stereo camera
+            res, disp = self.grab3D(isRectified)
+
+            # prepare images for displaying
+            display_image = np.zeros((640, 480), np.uint8)
+
+            rect_image_left_resized = self.stereo_camera.image_resize(self.rect_image_left, height=640)
+            rect_image_right_resized = self.stereo_camera.image_resize(self.rect_image_right, height=640)
+
+            disp_resized = self.scale_disparity(self.stereo_camera.image_resize(disp, height=640))
+            left_right_dual = np.concatenate((rect_image_left_resized, rect_image_right_resized), axis=1)
+
+            (lr_dual_h,lr_dual_w) = left_right_dual.shape
+            (d_h,d_w) = disp_resized.shape
+
+            spacer_width_raw = (lr_dual_w - d_w)
+            if (spacer_width_raw % 2) == 0:
+                #even
+                spacer_width_1 = int((spacer_width_raw / 2))
+                spacer_width_2 = int((spacer_width_raw / 2))
+            else:
+                #odd
+                spacer_width_1 = int((spacer_width_raw / 2))
+                spacer_width_2 = int((spacer_width_raw / 2) + 1)
+
+            disp_spacer_1 = np.zeros((640, spacer_width_1), np.uint8)
+            disp_spacer_2 = np.zeros((640, spacer_width_2), np.uint8)
+            disp_spaced = np.concatenate((disp_spacer_1, disp_resized, disp_spacer_2), axis=1)
+
+            display_image = np.concatenate((disp_spaced, left_right_dual), axis=0)
+            display_image_resize = self.stereo_camera.image_resize(display_image, height=640)
+            
+            # display disparity with stereo images
+            cv2.imshow(self.cv_window_name_Images, display_image_resize)
+
+            k = cv2.waitKey(1)          
+            if k == ord('q'): # exit if 'q' key pressed
+                break
+            elif k == ord('s'): # save stereo image pair
+                left_file_string=str(save_index)+"_l.png"
+                right_file_string=str(save_index)+"_r.png"
+                self.stereo_camera.save_images(self.image_left,self.image_right,defaultSaveFolder,left_file_string,right_file_string)
+                save_index += 1
+            elif k == ord('r'): # save rectified stereo image pair
+                left_file_string="rect_"+str(save_index)+"_l.png"
+                right_file_string="rect_"+str(save_index)+"_r.png"
+                self.stereo_camera.save_images(self.rect_image_left,self.rect_image_right,defaultSaveFolder,left_file_string,right_file_string)
+                save_index += 1
+            elif k == ord('p'): # save 3D data as point cloud
+                points_file_string = "points_"+str(save_index)+".ply"
+                self.save_point_cloud(disp,self.rect_image_left,defaultSaveFolder,points_file_string)
+                save_index += 1
+            elif k == ord('1'): # change tp OpenCV BM
+                self.change_matcher("BM")
+            elif k == ord('2'): # change to OpenCV SGBM
+                self.change_matcher("SGBM")
+            if cv2.getWindowProperty(self.cv_window_name_Images,cv2.WND_PROP_VISIBLE) < 1:        
+                break
+            if cv2.getWindowProperty(self.cv_window_name_Controls,cv2.WND_PROP_VISIBLE) < 1:        
+                break
+            time.sleep(frame_delay)
 
 if __name__ == "__main__":
     CAMERA_TYPE_PHOBOS = 0
@@ -293,25 +293,67 @@ if __name__ == "__main__":
     camera_type = CAMERA_TYPE_DEIMOS
 
     stcap = None
+    camera_name = None
     if (camera_type == CAMERA_TYPE_PHOBOS):
+        camera_name = "phobos"
         stcap = StereoCapture("Phobos",["22864917","22864912"])
     elif (camera_type == CAMERA_TYPE_PYLON):
         stcap = StereoCapture("Pylon",["22864917","22864912"])
     elif (camera_type == CAMERA_TYPE_DEIMOS):
+        camera_name = "deimos"
         stcap = StereoCapture("Deimos",0)
     elif (camera_type == CAMERA_TYPE_IMAGE):
-        stcap = StereoCapture("Image",["data/left.png","data/right.png"])
+        camera_name = "deimos"
+        stcap = StereoCapture("Image",["../SampleData/deimos_left.png","../SampleData/deimos_right.png"])
     else:
         print("Invalid camera type.")
         exit()
 
     # define inout folder
     folder = "../SampleData/"
-    # define calibration files for left and right image
-    left_cal_file = folder + "deimos_left.yaml"
-    right_cal_file = folder + "deimos_right.yaml"
+
+    CAL_MODE_FROM_IMAGES = 0
+    CAL_MODE_FROM_YAML = 1
+    CAL_MODE_FROM_XML = 2
+
+    stcal = None
+    cal_mode = CAL_MODE_FROM_XML
+    if (cal_mode == CAL_MODE_FROM_IMAGES):
+        # define calibration directories
+        left_images_folder = folder + "deimos_cal/"
+        right_images_folder = folder + "deimos_cal/"
+        output_folder = folder + "deimos_cal/"
+        left_wildcard = "*_l.png"
+        right_wildcard = "*_r.png"
+        grid_size = 39.0
+        grid_rows = 6
+        grid_cols = 8
+        # generate calibration from images
+        stcal = StereoCalibration()
+        stcal.calibrate(
+            left_images_folder,right_images_folder,
+            output_folder,left_wildcard,right_wildcard,
+            grid_size, grid_rows, grid_cols
+        )
+    elif (cal_mode == CAL_MODE_FROM_YAML):
+        # define calibration files for left and right image
+        left_cal_file = folder + camera_name +"_left.yaml"
+        right_cal_file = folder + camera_name +"_right.yaml"
+        # get calibration from yaml files
+        stcal = StereoCalibration()
+        stcal.get_cal_from_yaml(left_cal_file,right_cal_file)
+    elif (cal_mode == CAL_MODE_FROM_XML):
+        # define calibration files for left and right image
+        left_cal_file = folder + camera_name +"_left_calibration.xml"
+        right_cal_file = folder + camera_name +"_right_calibration.xml"
+        stereo_cal_file = folder + camera_name +"_stereo_calibration.xml"
+        left_rect_file = folder + camera_name +"_left_rectification.xml"
+        right_rect_file = folder + camera_name +"_right_rectification.xml"
+        # get calibration from yaml files
+        stcal = StereoCalibration()
+        stcal.get_cal_from_xml(left_cal_file,right_cal_file,stereo_cal_file,left_rect_file,right_rect_file)
 
     # setup Stereo3D
-    s3D = Stereo3D(left_cal_file,right_cal_file,stcap)
+    s3D = Stereo3D(stcap,stcal,"BM")
     # run Stereo3D GUI for generating 3D
     s3D.run(folder)
