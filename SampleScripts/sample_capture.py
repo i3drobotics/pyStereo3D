488a26d224c68d24c03f3d08b5248f4012debc82
Ben Knight <bknight@i3drobotics.com>
2020-04-29 14:53:15 +0100

    update directories in sample scripts

--- SampleScripts/sample_capture.py
+++ SampleScripts/sample_capture.py
@@ -21,21 +21,18 @@ elif (camera_type == CAMERA_TYPE_DEIMOS):
     stcap = StereoCapture("Deimos",0)
 elif (camera_type == CAMERA_TYPE_IMAGE):
     camera_name = "deimos"
-    stcap = StereoCapture("Image",["../../SampleData/deimos_left.png","../../SampleData/deimos_right.png"])
+    stcap = StereoCapture("Image",["SampleData/deimos_left.png","SampleData/deimos_right.png"])
 elif (camera_type == CAMERA_TYPE_VREP):
-    left_api_port = 20000
+    api_port = 20000
     left_vision_sensor_name = "StereoCameraLeft"
-    right_api_port = 20001
     right_vision_sensor_name = "StereoCameraRight"
-    camL = VREPCapture(left_api_port,left_vision_sensor_name)
-    camR = VREPCapture(right_api_port,right_vision_sensor_name)
-    stcapVREP = StereoCaptureVREP(camL,camR)
+    stcapVREP = StereoCaptureVREP(left_vision_sensor_name,right_vision_sensor_name,api_port)
     stcap = StereoCapture(stcapVREP)
 else:
     print("Invalid camera type.")
     exit()
 
 # define inout folder
-folder = "../../SampleData/"
+folder = "SampleData/"
 
 stcap.runGui(folder)
